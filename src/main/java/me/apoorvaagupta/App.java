9770f549d56c3d5e318e9c07a97e444b8c516308
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-15 23:29:11 +0530

    Finally Working! Remember to keep the dependencies up to date!!!
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- src/main/java/me/apoorvaagupta/App.java
+++ src/main/java/me/apoorvaagupta/App.java
@@ -1,5 +1,9 @@
 package me.apoorvaagupta;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
 /**
  * Hello world!
  */
@@ -8,6 +12,18 @@ public class App {
 
     public static void main(String[] args) {
 
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DbConfig.class);
+
+        TestMapper t = context.getBean(TestMapper.class);
+//        Test t1 = new Test(1, "t1");
+//        System.out.println(t1.getTestId());
+//        t.insertPerson(t1);
+        //t.insertPerson(new Test(5, "test2"));
+//        t.insertPerson(new Test(3, "test3"));
+        System.out.println(t.selectPerson(5).getTestId() + " " + t.selectPerson(5).getTestName());
+
+        //t.selectPerson(4);
+        //System.out.println(t4.getTestName());
 
     }
 }
